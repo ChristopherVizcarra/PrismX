4e6eaefa28bcdf304d78da65ae9dea1550e871d7
Dominic <dominic.truelen@gmail.com>
2015-05-18 22:44:38 +0800

    Fixed bug in conditionals

--- Parser1.py
+++ Parser1.py
@@ -7,10 +7,6 @@ class algorithm():
 	def set_input(self, temp):
 		self.input = temp
 		self.input = self.input.split("\n")
-		for x in self.input:
-			if len(x) == 0:
-				self.input.remove(x)
-
 		self.check()
 
 	def check(self):
@@ -19,31 +15,35 @@ class algorithm():
 			try:
 				temp = self.input[a]
 				temp = temp[:-1]
-				temp1 = list(temp.replace(" ", "").replace("\t", ""))
-				if temp.count("\t") < self.condition:
-					while temp.count("\t") < self.condition:
-						self.condition -= 1
+				temp1 = temp.lstrip()
+				temp1 = temp1.split(" ")
+
 			except:
 				break
 
+			if temp.count("\t") < self.condition:
+				while temp.count("\t") < self.condition:
+					self.condition -= 1
 
-			if temp1 == []:	#if line is empty, do nothing
+			if temp1 == [""]:	#if line is empty, do nothing
 				pass
 
-			elif temp1[1] == "=" and temp.count("\t") == self.condition:	#if line is assignment, assign
+			elif len(temp1) > 1 and temp1[1] == "=" and self.condition == temp.count("\t"):	#if line is assignment, assign
 				self.equate(temp1[0], temp1[2:])
 
-			elif temp1[0] == "i" and temp1[1] == "f" and temp1[2] == "(" and temp.count("\t") == self.condition:	#if line is if
-				temp2 = temp1[3:]
-				temp2 = "".join(temp2)
+			elif temp1[0] == "if" and "(" in temp1[1] and self.condition == temp.count("\t"):	#if line is if
+				temp2 = temp1[1].split("(")
+				temp2.remove("")
+				temp2 += temp1[2:]
 				self.conditional(temp2)
 
-			elif temp1[0] == "b" and temp1[1] == "r" and temp1[2] == "e" and temp1[3] == "a" and temp1[4] == "k" and len(temp1) == 5:
-				self.condition -= 1			
-			#elif
+			elif temp1[0] == "break" and len(temp1) == 1 and self.condition == temp.count("\t"):	#break
+				self.condition -= 1		
+
+			elif temp1[0] == "for" and "(" in temp1[1] and self.condition == temp.count("\t"):	#for loop
+				pass
 
 			a += 1
-
 
 	def equate(self, thisisvariablenumber1, thisisvariablenumber2):
 		thisisvariablenumber2 = list(thisisvariablenumber2)
@@ -61,7 +61,7 @@ class algorithm():
 
 	def conditional(self, thisisvariablenumber1):
 		try:
-			eval("".join([str(i) for i in thisisvariablenumber1]))
+			eval(" ".join([str(i) for i in thisisvariablenumber1]))
 
 		except:
 			thisisvariablenumber1 = list(thisisvariablenumber1)
@@ -69,18 +69,17 @@ class algorithm():
 				if thisisvariablenumber1[x] in self.variables:
 					thisisvariablenumber1[x] = self.variables[thisisvariablenumber1[x]]
 
-
-		if eval("".join([str(i) for i in thisisvariablenumber1])) == True:
+		if eval(" ".join([str(i) for i in thisisvariablenumber1])) == True:
 			self.condition += 1
 		else:
 			self.condition = 0
 
 x = algorithm()
 x.set_input("""
-z=7;
-y=z;
+z = 7;
+y = z;
 a = z * y;
-if (z == 7)
+if (z == 7 and y == 7)
 	w = 8;
 	break;
 	w = 100;
