1c5053888ce75ebdc5b215d4ea57445ac98bb53d
Dominic <dominic.truelen@gmail.com>
2015-05-19 15:43:21 +0800

    Added for loops.

--- Parser1.py
+++ Parser1.py
@@ -10,11 +10,14 @@ class algorithm():
 		self.input = self.input.split("\n")
 		self.check()
 
-	def check(self):
+	def check(self, b = None):
 		a = 0
 		while(1):
 			try:
-				temp = self.input[a]
+				if b == None:
+					temp = self.input[a]
+				else:
+					temp = b[a]
 				temp = temp[:-1]
 				temp1 = temp.lstrip()
 				temp1 = temp1.split(" ")
@@ -26,13 +29,14 @@ class algorithm():
 				while temp.count("\t") < self.condition:
 					self.condition -= 1
 
-			if self.condition != temp.count("\t"):
+			if self.condition != temp.count("\t") or temp1 == [""]:
 				pass
 
-			elif temp1 == [""]:	#if line is empty, do nothing
-				pass
-
-			elif len(temp1) > 1 and temp1[1] == "=":	#if line is assignment, assign
+			elif len(temp1) > 1 and "=" in temp1[1]:	#if line is assignment, assign
+				if len(temp1[1]) > 1:
+					temp1[1] = temp1[1].split("=")
+					temp1[1].remove("")
+					temp1[2:] = list(temp1[0]) + temp1[1] + temp1[2:]
 				self.equate(temp1[0], temp1[2:])
 
 			elif (temp1[0] == "if" or (temp1[0] == "elif" and self.trial[self.condition] == -1)) and "(" in temp1[1]:	#if line is if
@@ -48,7 +52,10 @@ class algorithm():
 				self.condition -= 1		
 
 			elif temp1[0] == "for" and "(" in temp1[1]:	#for loop
-				pass
+				temp2 = " ".join([str(i) for i in temp1])
+				temp2 = temp2.split("(")
+				temp2[1] = temp2[1].split(";")
+				self.forer(temp2[1], a)
 
 			a += 1
 
@@ -66,25 +73,52 @@ class algorithm():
 
 		print self.variables
 
-	def conditional(self, thisisvariablenumber1):
-		try:
-			eval(" ".join([str(i) for i in thisisvariablenumber1]))
+	def conditional(self, thisisvariablenumber1, thisisvariablenumber2=0):
+		thisisvariablenumber1 = list(thisisvariablenumber1)
+		for x in range(len(thisisvariablenumber1[1:])):
+			if thisisvariablenumber1[x] in self.variables:
+				thisisvariablenumber1[x] = self.variables[thisisvariablenumber1[x]]
 
-		except:
-			thisisvariablenumber1 = list(thisisvariablenumber1)
-			for x in range(len(thisisvariablenumber1[1:])):
-				if thisisvariablenumber1[x] in self.variables:
-					thisisvariablenumber1[x] = self.variables[thisisvariablenumber1[x]]
-
-		if eval(" ".join([str(i) for i in thisisvariablenumber1])) == True:
+		if eval(" ".join([str(i) for i in thisisvariablenumber1])) == True and thisisvariablenumber2 == 0:
 			self.trial[self.condition] = self.condition
 			self.condition += 1
+		elif eval(" ".join([str(i) for i in thisisvariablenumber1])) == True and thisisvariablenumber2 == 1:
+			return self.condition
+		elif eval(" ".join([str(i) for i in thisisvariablenumber1])) == True and thisisvariablenumber2 == 2:
+			return True
 		else:
 			self.trial[self.condition] = -1
 
+	def forer(self, thisisvariablenumber1, linenumber):
+		self.equate(thisisvariablenumber1[0].split(" ")[0], thisisvariablenumber1[0].split(" ")[2:])
+
+		temp = self.input[linenumber+1:]
+
+		while (self.conditional(thisisvariablenumber1[1].lstrip().split(" "), 2) == True):
+			self.condition += 1
+			self.trial[self.condition] = self.condition
+			self.check(temp)
+			self.iterate(thisisvariablenumber1[2])
+
+	def iterate(self, thisisvariablenumber1):
+		temp1 = thisisvariablenumber1.lstrip()
+		temp1 = temp1.split(" ")
+
+		temp2 = temp1[-1:]
+		temp1 = temp1[:-1]
+		temp1 = " ".join([str(i) for i in temp1])
+
+		if temp2 == ["++"]:
+			self.variables[temp1] += 1
+
+		elif temp2 == ["--"]:
+			self.variables[temp1] -= 1
+
+		#elif temp2 == "''"
+
 x = algorithm()
 x.set_input("""
-z = 8;
+z = 9;
 y = 9;
 a = z * y * 0;
 if (z == 7 and y == 7)
@@ -98,4 +132,10 @@ elif (z == 8)
 		w = 100;
 else
 	w = 5000;
+	b = w / 500 * 10;
+
+c = 0;
+
+for (x = 0; x < 10; x ++)
+	c += 1;
 """)
