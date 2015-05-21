e16f25bc98ce30f89ef835ec9aac4884fcfd8ef4
Dominic <dominic.truelen@gmail.com>
2015-05-21 17:14:35 +0800

    Added functions.

--- Parser1.py
+++ Parser1.py
@@ -1,16 +1,19 @@
+datatypes = ["boolean", "char", "int", "long", "float", "double"]
+
 class algorithm():
 	def __init__(self):
 		self.input = ""
 		self.variables = {}
 		self.condition = 0	#Determines how deep the code is at
 		self.trial = {}
+		self.functions = {}
 
 	def set_input(self, temp):
 		self.input = temp
 		self.input = self.input.split("\n")
 		self.check()
 
-	def check(self, b = None):
+	def check(self, b=None, c=None):
 		a = 0
 		while(1):
 			try:
@@ -32,12 +35,15 @@ class algorithm():
 			if self.condition != temp.count("\t") or temp1 == [""]:
 				pass
 
-			elif len(temp1) > 1 and "|" in temp1[0] and "|" in temp1[-1]:	#if line is assignment, assign
+			elif "|" in temp1[0] and "|" in temp1[-1]:	#if line is assignment, assign
 				temp1 = self.remove_pipe(temp1)
 				temp1 = self.split_equals(temp1)
 				temp1 = self.remove_greater(temp1)
 	
-				self.equate(temp1[0], temp1[2:])
+				if c == None:
+					self.equate(temp1[0], temp1[2:])
+				else:
+					self.equate(temp1[0], temp1[2:], c)
 
 			elif (temp1[0] == "if" or (temp1[0] == "elif" and self.trial[self.condition] == -1)) and "(" in temp1[1]:	#if line is if
 				self.conditional(temp1[1:])
@@ -59,6 +65,17 @@ class algorithm():
 
 			elif temp1[0] == "do" and len(temp1) == 1:
 				self.do_whiler(a)
+
+			elif temp1[0] in datatypes:
+				self.function_declare(temp1, a)
+
+			elif temp1[0] in self.functions:
+				temp1.remove("(")
+				temp1.pop()
+				temp2 = temp1[1:]
+				temp2 = "".join([str(i) for i in temp2])
+				temp2 = temp2.split(",")
+				self.run_function(temp1[0], temp2)
 
 			a += 1
 
@@ -98,7 +115,6 @@ class algorithm():
 		
 		return thisisvariablenumber1
 
-
 	def remove_greater(self, thisisvariablenumber1):
 		if len(thisisvariablenumber1[-1]) == 1 and ">" in thisisvariablenumber1[-1]:
 			thisisvariablenumber1.pop(-1)
@@ -114,15 +130,16 @@ class algorithm():
 
 		return thisisvariablenumber1
 
-
-	def equate(self, thisisvariablenumber1, thisisvariablenumber2):
+	def equate(self, thisisvariablenumber1, thisisvariablenumber2, extra=None):
 		thisisvariablenumber2 = list(thisisvariablenumber2)
 		try:
 			self.variables[thisisvariablenumber1] = eval("".join([str(i) for i in thisisvariablenumber2]))
 
 		except:
 			for x in range(len(thisisvariablenumber2)):
-				if thisisvariablenumber2[x] in self.variables:
+				if extra != None and thisisvariablenumber2[x] in extra:
+					thisisvariablenumber2[x] = extra[thisisvariablenumber2[x]] 
+				elif thisisvariablenumber2[x] in self.variables:
 					thisisvariablenumber2[x] = self.variables[thisisvariablenumber2[x]]
 			self.variables[thisisvariablenumber1] = eval("".join([str(i) for i in thisisvariablenumber2]))
 
@@ -220,9 +237,61 @@ class algorithm():
 
 		self.condition -= 1
 
+	def function_declare(self, thisisvariablenumber1, linenumber):
+		typer = thisisvariablenumber1[0]
+		name = thisisvariablenumber1[1]
+		thisisvariablenumber1.pop(2)
+		thisisvariablenumber1.pop(-1)
+		arguments = " ".join([str(i) for i in thisisvariablenumber1[2:]])
+		arguments = arguments.split(",")
+
+		temp = self.input[linenumber+1:]
+		temp1 = []
+		for x in temp:
+			if x.count("\t") == self.condition+1 and len(x) != 1:
+				temp1.append(x)
+			else:
+				break
+		temp = temp1
+		
+		self.functions[name] = [typer, arguments, temp]
+
+	def run_function(self, name, arguments=None):
+		temp = self.functions[name]
+		print temp
+		if len(temp[1]) != len(arguments):
+			print "ERROR. Invalid arguments."
+			exit(1)
+		else:
+			temp1 = {}
+			a = 0
+			for x in temp[1]:
+				temp2 = x.split(" ")
+				temp2[1] = " ".join([str(i) for i in temp2[1:]])
+				temp3 = temp2[0]
+				temp2 = temp2[1]
+				temp1[temp2] = arguments[a]
+				
+				if temp3 == "int":
+					temp1[temp2] = int(temp1[temp2])
+				elif temp3 == "float":
+					temp1[temp2] = float(temp1[temp2])
+				elif temp3 == "long":
+					temp1[temp2] = long(temp1[temp2])
+				elif temp3 == "double":
+					#temp1[temp2] = double(temp1[temp2])
+					pass
+				a += 1
+
+		self.condition += 1
+
+		self.check(temp[2], temp1)
 
 x = algorithm()
 x.set_input("""
+
+int adder ( int d ):
+	| d += 100 |;
 
 |z = 9|;
 |y = 9|;
@@ -239,15 +308,13 @@ elif ( z == 8 )
 else
 	|w = 5000|;
 	|b = <w / 500 * 10>|;
-
-
 | c=<0 + 100> |;
-
 for (|x = 0|; (x < 10); x ++)
 	|c += 1|;
-
 do:
 	|c -= 1|;
 while (c > 100);
+
+adder ( 5 );
 
 """)
